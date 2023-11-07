f987e8aadac3f8a477eeed783a4c86236fe5e2bd
thiendang201 <thiendang201@gmail.com>
2023-11-07 20:00:54 +0700

    update api

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
 import numpy as np
 import cv2
-import tensorflow as tf
-import pickle
 from skimage.feature import hog
 import joblib
 import json
@@ -34,15 +31,24 @@ def getPest(id):
 
   return {}
 
+@app.get('/pest/get')
+def get_pest(id):
+  for key in pests:
+    if pests[key]['ma_sau_benh'] == id:
+      return pests[key]
+
+  return {}
+
 @app.post('/predict')
 async def rice_leaf_pest_prediction(file: UploadFile = File(...)):
   contents = await file.read()
   jpg_as_np = np.frombuffer(contents, dtype=np.uint8)
-  image = cv2.imdecode(jpg_as_np, 0)
+  image = cv2.imdecode(jpg_as_np, cv2.IMREAD_COLOR)
   image_shape = (128, 128)
 
   #Resize ảnh về kích cỡ yêu cầu của mô hình
   image = cv2.resize(image, image_shape, interpolation = cv2.INTER_AREA)
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
   #Preprocessing dữ liệu về khoảng 0-1
   image = image/255.0
